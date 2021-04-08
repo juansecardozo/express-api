4edb7100817a2b14911b67345c130f6f3b2cd5cb
Sebastián Cardozo <sebastian.cardozo@consultingconnection.co>
2021-04-08 11:10:22 -0500

    Emitting messages through socket server

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,38 +1,49 @@
 const ValidationError = require("../../errors/ValidationError");
 const store = require("./store");
+const { socket } = require("../../socket");
 
 const addMessage = (user, message, chat, file) => {
-    let errors = [];
-    if (!user) {
-        console.error("[messageController] No user");
-        errors.push({ user: "User is required" });
-    }
-    if (!chat) {
-        console.error("[messageController] No chat");
-        errors.push({ chat: "Chat is required" });
-    }
-    if (!message) {
-        console.error("[messageController] No message");
-        errors.push({ message: "Message is required" });
-    }
-    if (errors.length) {
-        return Promise.reject(new ValidationError(errors));
-    }
+    return new Promise((resolve, reject) => {
+        let errors = [];
+        if (!user) {
+            console.error("[messageController] No user");
+            errors.push({ user: "User is required" });
+        }
+        if (!chat) {
+            console.error("[messageController] No chat");
+            errors.push({ chat: "Chat is required" });
+        }
+        if (!message) {
+            console.error("[messageController] No message");
+            errors.push({ message: "Message is required" });
+        }
+        if (errors.length) {
+            return reject(new ValidationError(errors));
+        }
 
-    let fileUrl = "";
-    if (file) {
-        fileUrl = "http://localhost:3000/public/files/" + file.filename;
-    }
+        let fileUrl = "";
+        if (file) {
+            fileUrl = "http://localhost:3000/public/files/" + file.filename;
+        }
 
-    const newMessage = {
-        user,
-        chat,
-        message,
-        file: fileUrl,
-        date: new Date(),
-    };
+        const newMessage = {
+            user,
+            chat,
+            message,
+            file: fileUrl,
+            date: new Date(),
+        };
 
-    return store.add(newMessage);
+        store
+            .add(newMessage)
+            .then((fullMessage) => {
+                socket.io.emit("message", fullMessage);
+                resolve(fullMessage);
+            })
+            .catch((e) => {
+                reject(e);
+            });
+    });
 };
 
 const getMessages = (chat, userFilter) => {
